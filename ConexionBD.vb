87e140c084db5c731c6a412446a0ba7d574f8f3a
leche <leche@DESKTOP-NU4UVLE>
2023-11-13 13:31:06 -0600

    Agregar archivos de proyecto.

---
+++ ConexionBD.vb
@@ -0,0 +1,8 @@
+﻿Imports System.Data.SqlClient
+Module ConexionBD
+    Public Function ObtenerConexion() As SqlConnection
+        Dim cadenaConexion As String = "Data Source='DESKTOP-NU4UVLE';Initial Catalog=INGSW;Integrated Security=True"
+        Dim conexion As New SqlConnection(cadenaConexion)
+        Return conexion
+    End Function
+End Module
